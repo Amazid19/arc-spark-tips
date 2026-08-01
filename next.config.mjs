e3dd5f96c433b612c6c8112bceec8a02302968d7
Amazid19 <82336474+Amazid19@users.noreply.github.com>
2026-08-01 23:02:51 +0600

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // Static Export Enable করার জন্য
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
