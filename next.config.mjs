cb36f33ffe23a0ebe726d08bff924aa5a856cfde
Amazid19 <82336474+Amazid19@users.noreply.github.com>
2026-08-01 23:34:50 +0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    // Build করার সময় টাইপস্ক্রিপ্ট এরর ইগনোর করবে
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Build করার সময় ESLint এরর ইগনোর করবে
+    ignoreDuringBuilds: true,
+  },
   webpack: (config) => {
-    config.externals.push('pino-pretty', 'lokijs', 'encoding', '@x402/svm');
+    config.externals.push(
+      'pino-pretty',
+      'lokijs',
+      'encoding',
+      '@x402/svm',
+      '@solana/web3.js'
+    );
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+      async_hooks: false,
+    };
     return config;
   },
 };
